13b2c3fd9a3dcbf2c66f01cbc5a8492fbe5dd847
Daniel Holanda <holand.daniel@gmail.com>
2025-09-10 06:42:21 -0700

    Update ROCm and Vulkan versions (#364)

--- src/lemonade/tools/llamacpp/utils.py
+++ src/lemonade/tools/llamacpp/utils.py
@@ -14,8 +14,8 @@ from lemonade.common.system_info import get_system_info
 
 from dotenv import set_key, load_dotenv
 
-LLAMA_VERSION_VULKAN = "b6392"
-LLAMA_VERSION_ROCM = "b1021"
+LLAMA_VERSION_VULKAN = "b6431"
+LLAMA_VERSION_ROCM = "b1057"
 
 
 def identify_rocm_arch_from_name(device_name: str) -> str | None:
