7c1d748cac7e8758d36ce39ba781d1d587db8b51
Daniel Holanda <holand.daniel@gmail.com>
2025-12-09 17:50:06 -0800

    Fetch the models list when the user clicks on the dropdown (#694)
    * Update LLM chat when models are downloaded/deleted
    
    * fetch the models list when the user clicks on the dropdown
    
    * Add back on load
    
    * minimal

--- src/app/src/renderer/ChatWindow.tsx
+++ src/app/src/renderer/ChatWindow.tsx
@@ -1089,6 +1089,7 @@ const sendMessage = async () => {
                   userHasSelectedModelRef.current = true;
                   setSelectedModel(e.target.value);
                 }}
+                onFocus={() => fetchModels()}
                 disabled={isLoading}
               >
                 {models.map((model) => (
