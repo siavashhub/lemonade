714772b6530be16ccf742366a308af13e6c6e2ad
Mario Limonciello <superm1@kernel.org>
2026-02-06 21:10:54 -0600

    Add a redirection to web app for mobile users (#1060)

--- src/app/src/renderer/index.tsx
+++ src/app/src/renderer/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../../assets/favicon.ico';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// Detect mobile user agents and redirect to appropriate app store
+const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+if (/android/i.test(userAgent)) {
+  window.location.href = 'https://play.google.com/store/apps/details?id=com.lemonade.mobile.chat.ai';
+} else if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+  window.location.href = 'https://apps.apple.com/ca/app/lemonade-mobile/id6757372210';
+} else {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
